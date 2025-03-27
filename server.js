c22ab72fc7bd7bb8c73838986162d88383fa25c8
Rafif Ramdhana <rafiframadhana.cs@gmail.com>
2025-03-27 22:48:48 +0700

    Use CORS

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import {products} from "./data/products.js";
+import { products } from "./data/products.js";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Use CORS middleware
+app.use(cors());
 
-app.get("/api/coffee", (req, res)=>{
-    res.json(products)
-})
+app.get("/api/coffee", (req, res) => {
+    res.json(products);
+});
 
-app.listen(PORT, ()=>{
-    console.log(`Server running on port ${PORT}`)
-})
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
