7c555d875fbed1e7fa0c3ee04da075e9fb314c91
Rafif Ramdhana <rafiframadhana.cs@gmail.com>
2025-03-28 10:31:10 +0700

    Add documentation

--- server.js
+++ server.js
@@ -7,13 +7,14 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Use CORS middleware
-app.use(cors());
+app.use(express.static("public"));
+
+app.use(cors({ origin: "*", methods: ["GET"] }));
 
 app.get("/api/coffee", (req, res) => {
-    res.json(products);
+  res.json(products);
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
